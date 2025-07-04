f76bd417145e00e8d2b16cce3a11739d642b2c9c
Niahoue <apacome343@gmail.com>
2025-07-04 20:05:00 +0000

    correction des mode

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,7 +7,7 @@ dotenv.config({ path: path.join(__dirname, '../.env') });
 module.exports = {
   // Configuration du serveur
   port: process.env.PORT || 5000,
-  nodeEnv: process.env. || 'development',
+  nodeEnv: process.env.MODE || 'development',
   
   // URL du frontend pour les liens dans les emails
   frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5174',
